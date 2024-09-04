6d71194d9c4f07ba0bca2acf9820c869714d89ac
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-09-04 17:12:43 -0300

    feat: Versão funcional já conectada com o banco

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -1,6 +1,19 @@
-import express from 'express';
+require('dotenv').config();
+
+const db = require("./db.js")
+const port = process.env.PORT;
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res)=>{
+    res.json({
+        message: "Funcionando!"
+    })
+})
+
+app.listen(port, () =>{
+    console.log(`Servidor rodando na porta ${port}`);
+})
