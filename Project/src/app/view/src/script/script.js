159fa2c3d6263343b3f65c1f3b19ce1e3c2f5615
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-09-18 19:42:03 -0300

    feat: Metodo POST implementado

--- Project/src/app/view/src/script/script.js
+++ Project/src/app/view/src/script/script.js
@@ -1,12 +1,50 @@
-const url = 'http://localhost:5500/usuario';
+const url = 'http://localhost:3000/usuario';
 
-const nome = document.getElementById('nome');
-const dataDeNascimento = document.getElementById('dataDeNascimento');
-
-async function getAllUsers(){
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+function getAllUsers(){
+    fetch(url)    
+    .then(res => res.json())
+    .then(data => {
+        data.map((users)=>{
+            const tr = document.createElement('tr');
+            const tdNome = document.createElement('td');
+            const tdDataDeNascimento = document.createElement('td');
+    
+            tdNome.innerText = users.nome;
+            tdDataDeNascimento.innerText = users.datadenascimento;
+    
+            tr.appendChild(tdNome);
+            tr.appendChild(tdDataDeNascimento);
+    
+            usersContainers.appendChild(tr);
+        })
+    })
+    .catch(error => {
+        console.log('Erro: ', error)
+    })
 }
 
 getAllUsers()
+
+function AddUsers(){
+    const nome = document.getElementById('nome').value;
+    const dataDeNascimento = document.getElementById('dataDeNascimento').value;
+    
+    fetch(url,{
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            dataDeNascimento: dataDeNascimento
+        })
+    })
+
+    .then(res => res.json())
+    .then(ret => {
+        console.log(ret);
+    })
+    .catch(error => {
+        console.log('Error: ', error)
+    })
+}
