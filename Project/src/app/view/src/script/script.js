697fb0ddee442e7a3bcaf682c3b7d90f4fb81646
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-09-20 02:42:35 -0300

    feat: Implementação de deletar e Atualizar na base de dados

--- Project/src/app/view/src/script/script.js
+++ Project/src/app/view/src/script/script.js
@@ -6,14 +6,26 @@ function getAllUsers(){
     .then(data => {
         data.map((users)=>{
             const tr = document.createElement('tr');
+
             const tdNome = document.createElement('td');
             const tdDataDeNascimento = document.createElement('td');
+            const tdAcoes = document.createElement('td');
     
             tdNome.innerText = users.nome;
             tdDataDeNascimento.innerText = users.datadenascimento;
+            tdAcoes.innerHTML = 
+            `
+            <button type="button" class="btnOp" onClick="DeleteUsers(${users.id})">
+                eletar
+            </button>
+            <button type="button" class="btnOp" onClick="updateUsers(${users.id})">
+                Atualizar
+            </button>
+            `;
     
             tr.appendChild(tdNome);
             tr.appendChild(tdDataDeNascimento);
+            tr.appendChild(tdAcoes);
     
             usersContainers.appendChild(tr);
         })
@@ -46,5 +58,56 @@ function AddUsers(){
     })
     .catch(error => {
         console.log('Error: ', error)
+    })
+}
+
+function DeleteUsers(id){
+    fetch(`http://localhost:3000/usuario/${id}`,{
+        method: "DELETE",
+        headers:{
+            "Content-Type": "application/json"
+        }
+    })
+
+    .then(res => {
+        if(res.ok){
+            console.log('Sucesso na exclusão');
+            alert("Sucesso na exclusão");
+            location.reload(); 
+        }else{
+            console.log('Erro na Exclusão');
+        }
+    })
+    .catch(error => {
+        console.log('Error: ', error);
+    })
+}
+
+function updateUsers(id){
+    const nome = document.getElementById('nome').value;
+    const dataDeNascimento = document.getElementById('dataDeNascimento').value;
+    
+    fetch(`http://localhost:3000/usuario/${id}`,{
+        method: "PUT",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            dataDeNascimento: dataDeNascimento
+        })
+    })
+
+    .then(res => {
+        if(res.ok){
+            console.log('Sucesso na atualização');
+            alert("Sucesso na atualização");
+            location.reload(); 
+        }else{
+            console.log('Erro na atualização');
+        }
+    })
+    .catch(error => {
+        console.log('Error: ', error);
     })
 }
